5710c3b69ac4bcc5ab5156bc89c5d11475d49055
Sang Nguyen <sang.nnh.62cntt@ntu.edu.vn>
2023-02-19 17:19:42 +0700

    add

--- AboutMe/src/main/java/thuchanh/AboutMe.java
+++ AboutMe/src/main/java/thuchanh/AboutMe.java
@@ -33,12 +33,13 @@ public class AboutMe extends HttpServlet {
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter traVe = response.getWriter();
 
-		traVe.println("<h1>About Me</h1>");
-		traVe.println("<p>Ten: Nguyen Ngoc Hoai Sang</p>");
-		traVe.println("<p>Lop: 62_CNTT_CLC</p>");
-		traVe.println("<p>MSSV: 62133231</p>");
-		traVe.println("<p>Email: sang.nnh.62cntt@ntu.edu.vn</p>");
-		traVe.println("<p>Phone: 0376354610</p>");
+		traVe.println("<h1 style=\"width:140px;rotate:-90deg;margin-top: 70px;\">About Me</h1>");
+//		traVe.println("<img src=\"Image/ms.png\" alt=\"My Image\">");
+		traVe.println("<p style=\"margin-top:-100px;margin-left:110px;\">Ten: Nguyen Ngoc Hoai Sang</p>");
+		traVe.println("<p style=\"margin-left:110px;\">Lop: 62_CNTT_CLC</p>");
+		traVe.println("<p style=\"margin-left:275px;margin-top:-35px;\">MSSV: 62133231</p>");
+		traVe.println("<p style=\"margin-left:110px;\">Email: sang.nnh.62cntt@ntu.edu.vn</p>");
+		traVe.println("<p style=\"margin-left:110px;\">Phone: 0376354610</p>");
 
 	}
 
